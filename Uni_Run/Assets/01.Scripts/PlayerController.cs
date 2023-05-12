60574eaafc7b7e87d3679b8014e79ab8e2f88cc6
ju0928 <gg2222222@naver.com>
2023-05-12 12:40:50 +0900

    20230512

--- Uni_Run/Assets/01.Scripts/PlayerController.cs
+++ Uni_Run/Assets/01.Scripts/PlayerController.cs
@@ -21,19 +21,58 @@ public class PlayerController : MonoBehaviour
         playerAudio = GetComponent<AudioSource>();
     }
 
-    private void Update()
+    private void Update() // 사용자 입력을 감지하고 점프하는 처리
     {
-        // 사용자 입력을 감지하고 점프하는 처리
+        if (isDead)
+        {
+            // 사망 시 처리를 더 이상 지행하지 않고 종료
+            return;
+        }
+            // 마우스 왼쪽 버튼을 누렀으며 && 최대 점프 횟수(2)에 도달하지 않았다면
+            if (Input.GetMouseButtonDown(0) && jumpConunt <2)
+            {
+                // 점프 횟수 증가
+                jumpConunt++;
+                // 점프 직전에 속도를 순간적으로 제로(0,0)로 변경
+                playerRigidbody.velocity = Vector2.zero;
+                // 리지드바디에 위쪽으로 힘 주기
+                playerRigidbody.AddForce(new Vector2(0, jumpForce));
+                // 오디오 소스 재생
+                playerAudio.Play();
+            }
+            else if (Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y >0)
+            {
+                // 마우스 왼쪽 버튼에서 손으 떼는 순간 && 속도의 y 값이 양수라면(위로 상승 중)
+                // 현재 속도를 절반으로 변경
+                playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
+            } 
+        
+        // 애니메이터의 Grounded 피라미너를 isGounded 값으로 갱신
+        animator.SetBool("Grounded", isGrounded);
     }
 
-    private void Die()
+    private void Die() // 사망 처리
     {
-        // 사망 처리
+        //애니메이터의 Die 트리거 파라미터를 셋
+        animator.SetTrigger("Die");
+        //오디오 소스에 할당된 오디오 클립을 deathClip으로 변경
+        playerAudio.clip = deathClip;
+        //tkakd gyrhkdma wotod
+        playerAudio.Play();
+
+        // 속도를 제로(0,0)로 변경
+        playerRigidbody.velocity = Vector2.zero;
+        //사망 상태로 true로 변경
+        isDead = true;
     }
     
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other) //트리거 콜라이더를 가진 장애물과의 충돌을 감지
     {
-      //트리거 콜라이더를 가진 장애물과의 충돌을 감지  
+      if (other.tag == "Dead"&&!isDead)
+        {
+            // 충돌한 상대방의 태그가 Dead이며 아직 사망하지 않았다면 Die() 실행
+            Die();
+        }
     }
    
     private void OnTriggerEnter2D(Collider2D collision)
